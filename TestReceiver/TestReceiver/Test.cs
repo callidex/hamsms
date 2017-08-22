edf78398f295ed809360eba533b5bf2e7f7c7b4b
callidex <callidex@users.noreply.github.com>
2017-08-22 14:09:22 +1000

    First commit (from TFS)

---
+++ TestReceiver/TestReceiver/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestReceiver
+{
+    class Test
+    {
+
+        void Test()
+        {
+            rex.Data += new RecorderEx.DataEventHandler(rex_Data);
+            rex.Open += new EventHandler(rex_Open);
+            rex.Close += new EventHandler(rex_Close);
+            rex.Format = pcmFormat;
+            rex.StartRecord();
+            Console.WriteLine("Please press enter to exit!");
+            Console.ReadLine();
+            rex.StopRecord();
+        }
+
+        static RecorderEx rex = new RecorderEx(true);
+        static PlayerEx play = new PlayerEx(true);
+        static IntPtr pcmFormat = AudioCompressionManager.GetPcmFormat(1, 16, 44100);
+
+        static void rex_Open(object sender, EventArgs e)
+        {
+            play.OpenPlayer(pcmFormat);
+            play.StartPlay();
+        }
+
+        static void rex_Close(object sender, EventArgs e)
+        {
+            play.ClosePlayer();
+        }
+
+        static void rex_Data(object sender, DataEventArgs e)
+        {
+            byte[] data = e.Data;
+            play.AddData(data);
+        }
+    }
+}
